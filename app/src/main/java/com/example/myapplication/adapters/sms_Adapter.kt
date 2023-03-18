2fb0f38de22bebfc561afddd717c212d9f679938
mac <bekaovkx@gmail.com>
2023-03-18 21:57:09 +0500

    USSD

--- app/src/main/java/com/example/myapplication/adapters/sms_Adapter.kt
+++ app/src/main/java/com/example/myapplication/adapters/sms_Adapter.kt
@@ -9,22 +9,21 @@ import com.example.myapplication.R
 import com.example.myapplication.databinding.ItemSmsBinding
 import com.example.myapplication.databinding.ItemTarifBinding
 
-class sms_Adapter(var list: ArrayList<UserTarif>, var context: Context) : RecyclerView.Adapter<sms_Adapter.Vh>() {
-    inner  class Vh(var itemTarifBinding: ItemTarifBinding):RecyclerView.ViewHolder(itemTarifBinding.root) {
-        fun onBind(userTarif: UserTarif, position: Int) {
+class sms_Adapter(var list: ArrayList<UserSms>, var context: Context) : RecyclerView.Adapter<sms_Adapter.Vh>() {
+    inner  class Vh(var itemTarifBinding:ItemSmsBinding):RecyclerView.ViewHolder(itemTarifBinding.root) {
+        fun onBind(userTarif: UserSms, position: Int) {
             val animation = AnimationUtils.loadAnimation(context, R.anim.rv_anim)
-            itemTarifBinding.animTarif.startAnimation(animation)
-            itemTarifBinding.tvName.text =userTarif.nomi
-            itemTarifBinding.tvPrice.text=  userTarif.narx
-            itemTarifBinding.tvDay.text=  userTarif.kun
-            itemTarifBinding.tvCodee.text=  userTarif.code
-            itemTarifBinding.info.text=  userTarif.info
+            itemTarifBinding.animSms.startAnimation(animation)
+            itemTarifBinding.tvPrice.text=  userTarif.price
+            itemTarifBinding.tvDay.text=  userTarif.day
+            itemTarifBinding.tvCountSms.text=  userTarif.count
+
             itemTarifBinding.imageUssd.setImageResource(R.drawable.beeline)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Vh {
-        return Vh(ItemTarifBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        return Vh(ItemSmsBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
     override fun onBindViewHolder(holder: Vh, position: Int) {
         holder.onBind(list[position] ,position)
--- app/src/main/java/com/example/myapplication/fragments/sms_Fragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/sms_Fragment.kt
@@ -13,7 +13,7 @@ import com.example.myapplication.adapters.sms_Adapter
 class sms_Fragment : Fragment() {
 
     private val binding by lazy { FragmentSmsBinding.inflate(layoutInflater) }
-    private lateinit var list_sms:ArrayList<UserSms>
+    private lateinit var list:ArrayList<UserSms>
     private lateinit var sms_Adapter: sms_Adapter
 
     override fun onCreateView(
@@ -23,7 +23,7 @@ class sms_Fragment : Fragment() {
 
         loadData()
 
-        sms_Adapter= sms_Adapter(list_sms , binding.root.context)
+        sms_Adapter= sms_Adapter(list , binding.root.context)
         binding.rvSms.adapter=sms_Adapter
 
         return binding.root
@@ -31,16 +31,16 @@ class sms_Fragment : Fragment() {
 
     private fun loadData() {
 
-        list_sms = ArrayList()
-        list_sms.add(UserSms(R.drawable.beeline, "20 SMS", "500 so'm", "Kun", "*110*161#"))
-        list_sms.add(UserSms(R.drawable.beeline, "50 SMS", "1000 so'm", "Kun", "*110*162#"))
-        list_sms.add(UserSms(R.drawable.beeline, "250 SMS", "1300 so'm", "Kun", "*110*151#"))
-        list_sms.add(UserSms(R.drawable.beeline, "100 SMS", "5263 so'm", "Oy", "*110*044#"))
-        list_sms.add(UserSms(R.drawable.beeline, "500 SMS", "13 683 so'm", "Oy", "*110*045#"))
-        list_sms.add(UserSms(R.drawable.beeline, "1000 SMS", "22 103 so'm", "Oy", "*110*046#"))
-        list_sms.add(UserSms(R.drawable.beeline, "25 SMS(xalqaro)", "5263 so'm", "-", "*110*041#"))
-        list_sms.add(UserSms(R.drawable.beeline, "50 SMS(xalqaro)", "8420 so'm", "-", "*110*042#"))
-        list_sms.add(UserSms(R.drawable.beeline, "100 SMS(xalqaro)", "12 630 so'm", "-", "*110*043#"))
+        list = ArrayList()
+        list.add(UserSms(R.drawable.beeline, "20 SMS", "500 so'm", "Kun", "*110*161#"))
+        list.add(UserSms(R.drawable.beeline, "50 SMS", "1000 so'm", "Kun", "*110*162#"))
+        list.add(UserSms(R.drawable.beeline, "250 SMS", "1300 so'm", "Kun", "*110*151#"))
+        list.add(UserSms(R.drawable.beeline, "100 SMS", "5263 so'm", "Oy", "*110*044#"))
+        list.add(UserSms(R.drawable.beeline, "500 SMS", "13 683 so'm", "Oy", "*110*045#"))
+        list.add(UserSms(R.drawable.beeline, "1000 SMS", "22 103 so'm", "Oy", "*110*046#"))
+        list.add(UserSms(R.drawable.beeline, "25 SMS(xalqaro)", "5263 so'm", "-", "*110*041#"))
+        list.add(UserSms(R.drawable.beeline, "50 SMS(xalqaro)", "8420 so'm", "-", "*110*042#"))
+        list.add(UserSms(R.drawable.beeline, "100 SMS(xalqaro)", "12 630 so'm", "-", "*110*043#"))
 
 
     }
